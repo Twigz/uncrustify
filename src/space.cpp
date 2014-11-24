8f8c97680049d617a83770f77c0aaf9839327c59
Twigz <aharris.1990@gmail.com>
2014-11-24 17:47:20 -0500

    Fixed some spacing issues

--- src/space.cpp
+++ src/space.cpp
@@ -699,7 +699,7 @@ static argval_t do_space(chunk_t *first, chunk_t *second, int& min_sp, bool comp
          return(cpd.settings[UO_sp_before_dc].a);
       }
       if ((second->type != CT_BYREF) &&
-          (second->type != CT_PTR_TYPE) && second->parent_type != CT_OC_MSG_DECL)
+          (second->type != CT_PTR_TYPE) && second->parent_type != CT_OC_MSG_DECL && second->type != CT_PAREN_CLOSE)
       {
          log_rule("sp_after_angle");
          return(cpd.settings[UO_sp_after_angle].a);
